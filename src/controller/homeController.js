04b9a104e0dd03760df836915b3f20aae0caaff2
lethangng <lethangng@email.com>
2022-08-16 19:53:45 +0700

    lesson 5

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,23 @@
+import connection from "../configs/connectDB"
 
 const getHomePage = (req, res) => {
     // logic
-    return res.render('index.ejs')
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users`',
+        function (err, results, fields) {
+            results.map((row) => {
+                data.push({
+                    id: row.id,
+                    email: row.email,
+                    address: row.address,
+                    firstName: row.firstName,
+                    lastName: row.lastName
+                })
+            });
+
+            return res.render('index.ejs', { dataUser: JSON.stringify(data) })
+        });
 }
 
 module.exports = {
