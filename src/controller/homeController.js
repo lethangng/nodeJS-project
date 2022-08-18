06dd45083e4fbd09bdd7b16f31dc96452be94123
lethangng <lethangng@email.com>
2022-08-18 15:10:36 +0700

    lesson 10

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,4 @@
+import multer from "multer";
 import pool from "../configs/connectDB"
 
 const getHomePage = async (req, res) => {
@@ -41,11 +42,44 @@ const postUpdateUser = async (req, res) => {
     return res.redirect('/')
 }
 
+const getUploadFilePage = async (req, res) => {
+    return res.render('uploadFile.ejs')
+}
+
+const upload = multer().single('profile_pic');
+
+const handleUploadFile = async (req, res) => {
+    // 'profile_pic' is the name of our file input field in the HTML form
+
+    upload(req, res, function (err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+    });
+}
+
 module.exports = {
     getHomePage,
     getDetailPage,
     createNewUser,
     deleteUser,
     getEditPage,
-    postUpdateUser
+    postUpdateUser,
+    getUploadFilePage,
+    handleUploadFile
 }
